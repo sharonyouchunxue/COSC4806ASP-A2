819f14a8b9e7e7bfead03d7d26e3bc3d8b0816d0
sharonyouchunxue <102676646+sharonyouchunxue@users.noreply.github.com>
2024-05-28 17:09:01 +0000

    database connection setup

--- database.php
+++ database.php
@@ -5,13 +5,10 @@ require_once('config.php');
 function db_connect(){
   try {
     $dbh = new PDO(
-      'mysql:host=' . DB_HOST . 
-      ';port=' . DB_PORT . 
-      ';dbname=' . DB_DATABASE, 
-      DB_USER, DB_PASSWORD
-    );
+      'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_DATABASE, DB_USER, DB_PASSWORD);
     return $dbh;
   } catch (PDOException $e) {
+    //we should set a global variable here so we know db is down
 
   }
 }
